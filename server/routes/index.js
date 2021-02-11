1fad4468ef17eb2a405af9827aceb94313e2f0d9
Jorge <jrglzt@gmail.com>
2021-02-11 01:03:31 -0500

    Borrado de carpeta server

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(require('./usuario'));
-app.use(require('./login'));
-app.use(require('./categoria'));
-
-
-module.exports = app;
